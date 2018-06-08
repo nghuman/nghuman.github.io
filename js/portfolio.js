671ce5f24360594903abae2491bb1aa53d2a3fdc
Navkiran <Navkiran@Jatt-MacBook-Pro.local>
2018-06-07 22:06:36 -0400

    fixed maddd things
    issa nice now

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,157 +1,56 @@
-!function($){
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-  var defaults = {
-    strength: 25,
-    scale: 1.05,
-    animationSpeed: "100ms",
-    contain: true,
-    wrapContent: false
-  };
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-  $.fn.interactive_bg = function(options){
-    return this.each(function(){
-      var settings = $.extend({}, defaults, options),
-      el = $(this),
-      h = el.outerHeight(),
-      w = el.outerWidth(),
-      sh = settings.strength / h,
-      sw = settings.strength / w,
-      has_touch = 'ontouchstart' in document.documentElement;
-
-      if (settings.contain == true) {
-        el.css({
-          overflow: "hidden"
-        });
-      }
-      // Insert new container so that the background can be contained when scaled.
-
-      if (settings.wrapContent == false) {
-        el.prepend("<div class='ibg-bg'></div>")
-      } else {
-        el.wrapInner("<div class='ibg-bg'></div>")
-      }
-
-
-
-      // Set background to the newly added container.
-
-      if (el.data("ibg-bg") !== undefined) {
-        el.find("> .ibg-bg").css({
-          background: "url('" + el.data("ibg-bg") + "') no-repeat center center",
-          "background-size": "cover",
-        });
-      }
-
-      el.find("> .ibg-bg").css({
-        width: w,
-        height: h
-      })
-
-
-
-      if(has_touch || screen.width <= 699) {
-        // For Mobile
-        // Add support for accelerometeron mobile
-        window.addEventListener('devicemotion', deviceMotionHandler, false);
-
-        function deviceMotionHandler(eventData) {
-          var accX = Math.round(event.accelerationIncludingGravity.x*10) / 10,
-          accY = Math.round(event.accelerationIncludingGravity.y*10) / 10,
-          xA = -(accX / 10) * settings.strength,
-          yA = -(accY / 10) * settings.strength,
-          newX = -(xA*2),
-          newY = -(yA*2);
-
-          el.find("> .ibg-bg").css({
-            "-webkit-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")",
-            "-moz-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")",
-            "-o-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")",
-            "transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")"
-          });
-
-        }
-
-      } else {
-        // For Desktop
-        // Animate only scaling when mouse enter
-        el.mouseenter(function(e) {
-          if (settings.scale != 1) el.addClass("ibg-entering")
-          el.find("> .ibg-bg").css({
-            "-webkit-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + ",0,0)",
-            "-moz-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + ",0,0)",
-            "-o-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + ",0,0)",
-            "transform": "matrix(" + settings.scale + ",0,0," + settings.scale + ",0,0)",
-            "-webkit-transition": "-webkit-transform " + settings.animationSpeed + " linear",
-            "-moz-transition": "-moz-transform " + settings.animationSpeed + " linear",
-            "-o-transition": "-o-transform " + settings.animationSpeed + " linear",
-            "transition": "transform " + settings.animationSpeed + " linear"
-          }).on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-
-            // This will signal the mousemove below to execute when the scaling animation stops
-            el.removeClass("ibg-entering")
-          });
-        }).mousemove(function(e){
-          // This condition prevents transition from causing the movement of the background to lag
-          if (!el.hasClass("ibg-entering") && !el.hasClass("exiting")) {
-            var pageX = e.pageX || e.clientX,
-            pageY = e.pageY || e.clientY,
-            pageX = (pageX - el.offset().left) - (w / 2),
-            pageY = (pageY - el.offset().top) - (h / 2),
-            newX = ((sw * pageX)) * - 1,
-            newY = ((sh * pageY)) * - 1;
-            // Use matrix to move the background from its origin
-            // Also, disable transition to prevent lag
-            el.find("> .ibg-bg").css({
-              "-webkit-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")",
-              "-moz-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")",
-              "-o-transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")",
-              "transform": "matrix(" + settings.scale + ",0,0," + settings.scale + "," + newX + "," + newY + ")",
-              "-webkit-transition": "none",
-              "-moz-transition": "none",
-              "-o-transition": "none",
-              "transition": "none"
-            });yg
-          }
-        }).mouseleave(function(e) {
-          if (settings.scale != 1) el.addClass("ibg-exiting")
-          // Same condition applies as mouseenter. Rescale the background back to its original scale
-          el.addClass("ibg-exiting").find("> .ibg-bg").css({
-            "-webkit-transform": "matrix(1,0,0,1,0,0)",
-            "-moz-transform": "matrix(1,0,0,1,0,0)",
-            "-o-transform": "matrix(1,0,0,1,0,0)",
-            "transform": "matrix(1,0,0,1,0,0)",
-            "-webkit-transition": "-webkit-transform " + settings.animationSpeed + " linear",
-            "-moz-transition": "-moz-transform " + settings.animationSpeed + " linear",
-            "-o-transition": "-o-transform " + settings.animationSpeed + " linear",
-            "transition": "transform " + settings.animationSpeed + " linear"
-          }).on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-            el.removeClass("ibg-exiting")
-          });
-        });
-      }
-    });
-
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-}(window.jQuery);
+  var that = this;
+  var delta = 300 - Math.random() * 100;
 
+  if (this.isDeleting) { delta /= 2; }
 
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
 
-$(document).ready(function(){
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
 
-  $(".bg").interactive_bg({
-    strength: 25,
-    scale: 1.05,
-    animationSpeed: "100ms",
-    contain: true,
-    wrapContent: false
-  });
-
-  $(window).resize(function() {
-    $(".bg > .ibg-bg").css({
-      width: $(window).outerWidth(),
-      height: $(window).outerHeight()
-    })
-  })
-})
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em }";
+  document.body.appendChild(css);
+};
